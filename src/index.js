15f1eab10d629b95bd80bfc63e2ea02ee5a5bc3c
Turebekov Arman <turebekov.arman>
2020-10-03 23:11:15 +0600

    feat: added a function that returns the avg value from an array

--- src/index.js
+++ src/index.js
@@ -25,5 +25,12 @@ exports.max = function max(array) {
 };
 
 exports.avg = function avg(array) {
-    return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length;
 };
