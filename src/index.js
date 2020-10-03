9dfc97b7d57db5f277ab8e634943c3462cb11ece
Turebekov Arman <turebekov.arman>
2020-10-03 23:08:49 +0600

    feat: added a function that returns the minimum value from an array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (min >= array[i]) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return 0;
+};
